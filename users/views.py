46fba3c95873c549ce73863d508d2d8ae75863d4
valed-dm <dmvaled@gmail.com>
2024-01-27 12:38:45 +0300

    users/views.py class-based views added
    RegisterView, LoginView, LogoutView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView as LoginViewContrib
+from django.contrib.auth.views import LogoutView as LogoutViewContrib
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import CustomAuthForm, RegisterForm
+from users.models import CustomUser
+
+
+class RegisterView(CreateView):
+    model = CustomUser
+    form_class = RegisterForm
+    template_name = "users/register.html"
+    success_url = "/"
+
+
+class LoginView(LoginViewContrib):
+    form_class = CustomAuthForm
+    template_name = "users/login.html"
+    success_url = "/"
+
+
+class LogoutView(LogoutViewContrib):
+    pass
